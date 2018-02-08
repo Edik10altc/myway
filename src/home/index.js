da467bf602f3e048b60686ccf59a1383d32e6777
liatrefael <36272456+liatrefael@users.noreply.github.com>
2018-02-08 21:38:48 +0200

    update repository

--- src/home/index.js
+++ src/home/index.js
@@ -1,41 +1,29 @@
-/**
- * React Static Boilerplate
- * https://github.com/kriasoft/react-static-boilerplate
- *
- * Copyright © 2015-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
+import React, { Component } from 'react';
+import RightBar from './RightBar';
+import LeftBar from './LeftBar';
+import MainBoard from './MainBoard';
+import logo from './logo.svg';
+import './App.css';
 
-import React, {PropTypes} from 'react';
-import s from './styles.css';
-import {title, html} from './index.md';
-import axios from 'axios';
 
-import history from '../history';
-class HomePage extends React.Component {
-
+class App extends Component {
   constructor(props) {
     super(props);
   }
 
-  componentDidMount() {
-    document.title = title;
-  }
 
-  gotToYelpCloneExample(){
-    history.push('/yelp-clone-example')
-  }
   render() {
     return (
-      <div className={s.container}>
-        <div dangerouslySetInnerHTML={{__html: html}}/>
-        <button onClick = {()=>{this.gotToYelpCloneExample()}}>Check Out Yelp Clone Example</button>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <LeftBar/>
+        <MainBoard/>
+        <RightBar/>
       </div>
-    );
-  }
-
+    )}
 }
 
 export default HomePage;
